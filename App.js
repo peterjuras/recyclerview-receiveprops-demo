66d15d069667a44d477bca90eefb09e98e16fb83
Peter Juras <me@peterjuras.com>
2017-09-07 17:55:00 +0200

    Reproduce RecyclerListView bug

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import { DataProvider, LayoutProvider, RecyclerListView } from 'recyclerlistview';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+
+function renderRow(_, data) {
+  return <Text>{data}</Text>;
+}
+
 export default class App extends React.Component {
+  dataProvider = new DataProvider((r1, r2) => r1 !== r2)
+    .cloneWithRows(Array.from(new Array(500)).map((_, index) => index));
+
+  layoutProvider = new LayoutProvider(
+    () => 1,
+    (_, dimensions) => {
+      dimensions.height = 50;
+      dimensions.width = 150;
+    }
+  );
+
+  componentDidMount() {
+    this.setState({
+      test: true
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <RecyclerListView
+          layoutProvider={this.layoutProvider}
+          dataProvider={this.dataProvider}
+          rowRenderer={renderRow}
+        />
       </View>
     );
   }
@@ -17,7 +42,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch'
   },
 });
